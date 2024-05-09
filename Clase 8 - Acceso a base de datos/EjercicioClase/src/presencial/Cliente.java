0bc9bedf8361d7e0be5a080c12ed8729ae039a19
OmarAlex24 <artcate208@gmail.com>
2024-05-09 17:48:09 -0600

    Add recursos/

---
+++ Clase 8 - Acceso a base de datos/EjercicioClase/src/presencial/Cliente.java
@@ -0,0 +1,76 @@
+import java.sql.*;
+import java.util.logging.Logger;
+
+public class Cliente {
+    private static final Logger logger = Logger.getLogger(Cliente.class);
+
+    private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(Cliente.class);
+
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        try {
+            connection = getConnection();
+
+            //Nos va a permitir comunicarnos con la db
+            Statement statement = connection.createStatement();
+
+            // Crear la tabla, borrarla si ya existe
+            statement.execute("DROP TABLE IF EXISTS ANIMALES; CREATE TABLE ANIMALES(ID INT PRIMARY KEY ,NOMBRE VARCHAR(20) NOT NULL,TIPO VARCHAR(20) NOT NULL)");
+
+            logger.info("Se creo correctamente la tabla en la bd");
+
+            // Ahora insertamos a los animales
+            statement.execute("INSERT INTO ANIMALES VALUES (1,'Angie','Delfin')," +
+                                                                "(2,'Lucas','Rana')," +
+                                                                "(3,'Saviolo','Conejo')," +
+                                                                "(4,'Garfield','Gato')," +
+                                                                "(5,'Oddy','Perro')");
+
+            logger.info("Se insertaron correctamente los animales en la tabla ANIMALES");
+
+            // Para mostrarlos necesito guardarlos en una clase ResultSet
+            ResultSet rs = statement.executeQuery("SELECT * FROM ANIMALES");
+
+            while (rs.next()){
+                System.out.println("ID: "+rs.getInt(1)+
+                                    "\nNombre: "+rs.getString(2)+
+                                    "\nTipo: "+rs.getString(3)+"\n");
+            }
+            System.out.println("**************************************");
+
+            Statement statement1 = connection.createStatement();
+
+
+            statement1.execute("DELETE FROM ANIMALES WHERE ID=2");
+            logger.warning("Se elimino el id 2");
+
+
+            ResultSet rs1 = statement1.executeQuery("SELECT * FROM ANIMALES");
+
+            while (rs1.next()){
+                System.out.println("ID: "+rs.getInt(1)+
+                        "\nNombre: "+rs.getString(2)+
+                        "\nTipo: "+rs.getString(3)+"\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                logger.warning("Se cerro correctamente la conexion con la bd");
+            }catch (SQLException e){
+                e.getMessage();
+            }
+        }
+    }
+
+    public static Connection getConnection() throws Exception{
+        //Estamos creando un metodo que devuelve la conexion
+        // y nos carga el driver necesario
+
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:~/C3Animales","sa","sa");
+    }
+}
