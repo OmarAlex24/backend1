12f185f0f42a01c8170bfedf489be50c9418e7a2
OmarAlex24 <artcate208@gmail.com>
2024-05-21 18:08:50 -0600

    Modify folder examen

--- Examen/src/dao/BD.java
+++ Examen/src/dao/BD.java
@@ -28,6 +28,6 @@ public class BD {
 
     public static Connection getConnection() throws Exception{
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/Examen","sa","sa");
+        return DriverManager.getConnection("jdbc:h2:mem:~/Examen","sa","sa");
     }
 }
